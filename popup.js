e0d82dd312e5f2484d0b7ee90fb3737fe3369621
Florian242 <floriancata04@gmail.com>
2021-03-24 17:32:10 +1100

    création content.js + adaptation du code dans Popup.js

--- popup.js
+++ popup.js
@@ -1,12 +1,36 @@
+var textProduction = document.createElement('p')
+textProduction.setAttribute("id","production")
+textProduction.style.color = 'red'
+textProduction.style.visibility="hidden"
+textProduction.innerText =' Vous êtes en environnement de production. Prudence !'
+document.body.append(textProduction)
+
+var textQualification = document.createElement('p')
+textProduction.setAttribute("id","qualification")
+textQualification.style.visibility="hidden"
+textQualification.style.color = 'red'
+textQualification.innerText =' Vous êtes en environnement de qualification. Veuillez à le garder en bon état pour les tests.'
+document.body.append(textQualification)
+
+var textDeveloppement = document.createElement('p')
+textProduction.setAttribute("id","developpement")
+textDeveloppement.style.visibility="hidden"
+textDeveloppement.style.color = 'red'
+textDeveloppement.innerText =' Vous êtes en environnement de développement.'
+document.body.append(textDeveloppement)
+
+
+
 function show_Standart(){
 
     // La procedure n'as pas de parametre
     // La procedure ne renvoie rien 
     // la procedure cache tous les messages
 
-    document.getElementById("qualification").style.display="none";
-    document.getElementById("developpement").style.display="none";
-    document.getElementById("production").style.display="none";                                                                                                        
+    textProduction.style.visibility="hidden";
+    textQualification.style.visibility="hidden";
+    textDeveloppement.style.visibility="hidden"; 
+                                                                                                           
 }     
 
 function show_Hide_Production(){
@@ -15,9 +39,10 @@ function show_Hide_Production(){
     // La procedure ne renvoie rien 
     // la procedure cache tous les messages sauf le message de production
 
-    document.getElementById("qualification").style.display="none";
-    document.getElementById("developpement").style.display="none";
-    document.getElementById("production").style.display="inline"; 
+    textProduction.style.visibility="visible";
+    textQualification.style.visibility="hidden";
+    textDeveloppement.style.visibility="hidden"; 
+    
 }
 
 function show_Hide_Qualification(){
@@ -26,9 +51,10 @@ function show_Hide_Qualification(){
     // La procedure ne renvoie rien 
     // la procedure cache tous les messages sauf le message de qualification
 
-    document.getElementById("production").style.display="none";
-    document.getElementById("developpement").style.display="none"; 
-    document.getElementById("qualification").style.display="inline";                 
+    textProduction.style.visibility="hidden";
+    textDeveloppement.style.visibility="hidden"; 
+    textQualification.style.visibility="visible";  
+                  
 }
 
 function show_Hide_Developpement(){
@@ -37,7 +63,8 @@ function show_Hide_Developpement(){
     // La procedure ne renvoie rien 
     // la procedure cache tous les messages sauf le message de développement
 
-    document.getElementById("production").style.display="none";
-    document.getElementById("qualification").style.display="none"; 
-    document.getElementById("developpement").style.display="inline";
+    textProduction.style.visibility="hidden";
+    textQualification.style.visibility="hidden"; 
+    textDeveloppement.style.visibility="visible";
+   
 }
